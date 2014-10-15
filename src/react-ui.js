012e532f3c7ba84a7d0ff1ce3880e7dc3b544589
Ivan Torres <mexpolk@gmail.com>
2014-10-15 06:35:53 -0500

    partial implementation select menu

--- src/react-ui.js
+++ src/react-ui.js
@@ -14,13 +14,25 @@
 
   var hasOwnProperty = {}.hasOwnProperty;
 
-  var extend = function (dest, src) {
-    for (var key in src) {
-      if (hasOwnProperty.call(src, key)) {
-        dest[key] = src[key];
-      }
-    }
-  };
+  // var extend = function (obj, props) {
+  //   for (var key in props) {
+  //     if (hasOwnProperty.call(props, key)) {
+  //       obj[key] = props[key];
+  //     }
+  //   }
+  // };
+  //
+  // var filter = function (obj, attrs) {
+  //   var filtered = {};
+  //
+  //   if (typeof attrs === 'string') {
+  //     attrs = [attrs];
+  //   }
+  //
+  //   attrs.forEach(function (key) {
+  //     filtered[key] = obj[key];
+  //   });
+  // };
 
   var dom = React.DOM;
 
@@ -29,7 +41,7 @@
     render: function () {
 
       var props = {
-        className: (this.props.className ? this.props.ClassName + ' ' : '') + 'react-ui-button'
+        className: (this.props.className ? this.props.ClassName + ' ' : '') + 'react-ui react-ui-button'
       };
 
       var buttonProps = {
@@ -47,7 +59,7 @@
     onClick: function (event) {
       event.preventDefault();
       if (typeof this.props.onClick === 'function') {
-        this.props.onClick(event);
+        this.props.onClick();
       }
     }
 
@@ -60,8 +72,9 @@
     },
 
     render: function () {
+
       var props = {
-        className: (this.props.className ? this.props.ClassName + ' ' : '') + 'react-ui-checkbox'
+        className: (this.props.className ? this.props.ClassName + ' ' : '') + 'react-ui react-ui-checkbox'
       };
 
       var checkboxProps = {
@@ -78,8 +91,6 @@
     },
 
     onClick: function (event) {
-      event.preventDefault();
-
       this.setState({
         'checked': !this.state.checked
       });
@@ -95,9 +106,151 @@
 
   });
 
+  var TextInput = React.createClass({
+
+    getInitialState: function () {
+      return {
+        value: this.props.value || ''
+      };
+    },
+
+    render: function () {
+
+      var props = {
+        className: (this.props.className ? this.props.ClassName + ' ' : '') + 'react-ui react-ui-text-input'
+      };
+
+      var buttonProps = {
+        type: 'text',
+        disabled: this.props.disabled || false,
+        title: this.props.title,
+        placeholder: this.props.placeholder,
+        onChange: (this.onChange || void 0),
+        onKeyUp: (this.onKeyUp || void 0)
+      };
+
+      return dom.div(props,
+        dom.input(buttonProps, this.props.children)
+      );
+
+    },
+
+    onChange: function (event) {
+      this.setState({
+        value: this.getDOMNode().querySelector('input').value
+      });
+
+      if (this.props.onChange) {
+        this.props.onChange(event);
+      }
+    },
+
+    onKeyUp: function (event) {
+      this.setState({
+        value: this.getDOMNode().querySelector('input').value
+      });
+
+      if (this.props.onKeyUp) {
+        var filteredEvent = {
+          value: this.state.value
+        };
+
+        this.props.onKeyUp(event);
+      }
+    }
+
+  });
+
+  var Select = React.createClass({
+
+    getInitialState: function () {
+      return {
+        value: this.props.value || void 0,
+        collapsed: true
+      };
+    },
+
+    render: function () {
+      var props = {
+        className: (this.props.className ? this.props.ClassName + ' ' : '') + 'react-ui react-ui-select',
+        onClick: this.onClick
+      };
+
+      if (this.state.collapsed) {
+        props.className += ' react-ui-select-is-collapsed';
+      }
+
+      var inputProps = {
+        onKeyPress: this.onKeyPress,
+        type: 'text',
+        value: this.getLabel()
+      };
+
+      return dom.div(props, [
+        dom.div({
+          className: 'react-ui-select-input'
+        }, [
+          dom.input(inputProps, this.getLabel()),
+        ]),
+        dom.div({
+          className: 'react-ui-select-caret'
+        }),
+        dom.ul(null, this.renderOptions())
+      ]);
+    },
+
+    getLabel: function () {
+      var label, option;
+      for (var i = 0, len = this.props.options.length; i < len; i++) {
+        option = this.props.options[i];
+        if (option.value == this.state.value) {
+          label = option.label;
+          break;
+        }
+      }
+      return label || '';
+    },
+
+    onClick: function (event) {
+      this.setState({ collapsed: !this.state.collapsed });
+    },
+
+    onKeyPress: function (event) {
+      event.preventDefault();
+    },
+
+    onOptionClick: function (value) {
+      this.setState({ value: value });
+      if (this.options.onChange) {
+        this.options.onChange(this.state.value);
+      }
+    },
+
+    renderOptions: function () {
+      return this.props.options.map(function (option, key) {
+        var props = {
+          key: key,
+          onClick: function () {
+            this.onOptionClick(option.value);
+          }.bind(this)
+        };
+
+        if (option.value === this.state.value) {
+          props.className = 'react-ui-select-option-is-selected';
+        }
+
+        return dom.li(props, option.label);
+
+      }.bind(this));
+    }
+
+  });
+
   return {
     Button: Button,
-    Checkbox: Checkbox
+    Checkbox: Checkbox,
+    Select: Select,
+    TextInput: TextInput
   };
 
 }));
